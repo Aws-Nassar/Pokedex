4028eeb791be1e54d3622b0e939cf529c6fb989c
aws-nassar <aws.a.nasssar@gmail.com>
2026-02-13 03:11:58 +0200

    CH2: L6

---
+++ src/command_inspect.ts
@@ -0,0 +1,39 @@
+import { types } from "node:util";
+import {State } from "./state.js";
+
+//catch call back function
+export async function commandInspect(state: State, ...args: string[]): Promise<void> {
+    if (!args[0])
+    {
+        console.log("Enter a Pokemon name");
+        return;
+    }
+
+    if (!(args[0] in state.pokedex))
+    {
+        console.log("you have not caught that pokemon");
+        return;
+    }
+
+    const pokemonInfo = await state.pokeapi.fetchPokemon(args[0]);
+
+    if (!pokemonInfo)
+    {   
+        console.log("Unknown Pokemon name");
+        return;
+    }
+
+    console.log(`Name: ${pokemonInfo.name}`);
+    console.log(`Height: ${pokemonInfo.height}`);
+    console.log(`Weight: ${pokemonInfo.weight}`);
+    console.log(`Stats:`);
+    for (const stat of pokemonInfo.stats) {
+        console.log(`  -${stat.stat.name}: ${stat.base_stat}`);
+    }
+    console.log(`Types:`);
+    for (const typee of pokemonInfo.types) {
+        console.log(`  -${typee.type.name}`);
+    }
+
+
+}
--- src/commands.ts
+++ src/commands.ts
@@ -5,6 +5,7 @@ import {commandMap} from "./command_map.js";
 import {commandMapb} from "./command_mapb.js";
 import {commandExplore} from "./command_explore.js";
 import {commandCatch} from "./command_catch.js";
+import {commandInspect} from "./command_inspect.js";
 
 
 
@@ -40,6 +41,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "catch",
       description: "Catch some Pokemon and add them to Pokedex",
       callback: commandCatch,
+    },
+    inspect: {
+      name: "inspect",
+      description: "Display the captured Pokemon/s info inside the Pokedex",
+      callback: commandInspect,
     },
   };
 }
